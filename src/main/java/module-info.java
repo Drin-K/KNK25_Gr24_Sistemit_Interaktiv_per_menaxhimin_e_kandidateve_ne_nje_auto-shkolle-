af8af885ca7ec0216d9f8b88feced810034b748a
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-03-21 20:31:37 +0100

    Start of Application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk25_gr24_sistemit_interaktiv_per_menaxhimin_e_kandidateve_ne_nje_autoshkolle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.knk25_gr24_sistemit_interaktiv_per_menaxhimin_e_kandidateve_ne_nje_autoshkolle to javafx.fxml;
+    exports com.example.knk25_gr24_sistemit_interaktiv_per_menaxhimin_e_kandidateve_ne_nje_autoshkolle;
+}

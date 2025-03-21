0333718186d5985dbe6ed16cd58103abc61d9628
mehmetmehmeti <memimehmet8@gmail.com>
2025-03-21 23:10:16 +0100

    lidhja_2

--- src/main/java/Database/DBConnector.java
+++ src/main/java/Database/DBConnector.java
@@ -5,18 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-    private static final String DB_URL="jdbc:postgresql://localhost/KNK_2025";
+    private static final String DB_URL="jdbc:postgresql://localhost/KNK_2022";
     private static final String DB_USER="postgres";
-    private static final String DB_USER_PASSWORD="12345678";
+    private static final String DB_PASSWORD="12345678";
+    private static Connection connection= null;
     public static Connection getConnection(){
-        if (connection==null){
-            try {
-                connection= DriverManager.getConnection(
-                        DB_URL,DB_USER,DB_USER_PASSWORD
-                );
-            } catch (SQLException e) {
+        if(connection==null){
+            try{
+                connection= DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            }
+            catch(SQLException e){
                 return null;
             }
         }
         return connection;
-}}
+    }
+}

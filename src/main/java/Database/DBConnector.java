0c85748e57b3252965b6efda6872109cea4cc070
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-03-21 23:00:38 +0100

    Start of Application

--- src/main/java/Database/DBConnector.java
+++ src/main/java/Database/DBConnector.java
@@ -1,4 +1,22 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
-}
+    private static final String DB_URL="jdbc:postgresql://localhost/KNK_2025";
+    private static final String DB_USER="postgres";
+    private static final String DB_USER_PASSWORD="12345678";
+    public static Connection getConnection(){
+        if (connection==null){
+            try {
+                connection= DriverManager.getConnection(
+                        DB_URL,DB_USER,DB_USER_PASSWORD
+                );
+            } catch (SQLException e) {
+                return null;
+            }
+        }
+        return connection;
+}}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.knk25_gr24_sistemit_interaktiv_per_menaxhimin_e_kandidateve_ne_nje_autoshkolle {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.knk25_gr24_sistemit_interaktiv_per_menaxhimin_e_kandidateve_ne_nje_autoshkolle to javafx.fxml;

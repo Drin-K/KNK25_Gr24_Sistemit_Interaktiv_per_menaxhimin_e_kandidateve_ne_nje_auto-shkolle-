2d0bde860538e84d2a8601f213a8e7bb5f199694
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-02 13:28:12 +0200

    Krijimi i UpdateKategorite_PatentesDto.java

--- src/main/java/models/Dto/Kategorite_Patentes/UpdateKategorite_PatentesDto.java
+++ src/main/java/models/Dto/Kategorite_Patentes/UpdateKategorite_PatentesDto.java
@@ -1,4 +1,13 @@
 package models.Dto.Kategorite_Patentes;
 
 public class UpdateKategorite_PatentesDto {
+    private String pershkrimi;
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public void setPershkrimi(String pershkrimi) {
+        this.pershkrimi = pershkrimi;
+    }
 }

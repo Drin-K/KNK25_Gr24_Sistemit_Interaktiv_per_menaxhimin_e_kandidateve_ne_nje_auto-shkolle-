2c912391a560cafbd17fffe64aa56616f79a6210
mehmetmehmeti <memimehmet8@gmail.com>
2025-04-02 13:40:40 +0200

    Krijimi i klases CreatePatentaDtoDto

--- src/main/java/models/Dto/Patenta/CreatePatentaDto.java
+++ src/main/java/models/Dto/Patenta/CreatePatentaDto.java
@@ -1,5 +1,60 @@
 package models.Dto.Patenta;
 
 public class CreatePatentaDto {
+    private int id_patente;
+    private int id_kandidat;
+    private int id_kategori;
+    private String data_leshimit;
+    private String statusi;
+
+    public CreatePatentaDto(int id_patente, int id_kandidat, int id_kategori, String data_leshimit, String statusi) {
+        this.id_patente = id_patente;
+        this.id_kandidat = id_kandidat;
+        this.id_kategori = id_kategori;
+        this.data_leshimit = data_leshimit;
+        this.statusi = statusi;
+    }
+
+    public int getId_patente() {
+        return id_patente;
+    }
+
+    public void setId_patente(int id_patente) {
+        this.id_patente = id_patente;
+    }
+
+    public int getId_kandidat() {
+        return id_kandidat;
+    }
+
+    public void setId_kandidat(int id_kandidat) {
+        this.id_kandidat = id_kandidat;
+    }
+
+    public int getId_kategori() {
+        return id_kategori;
+    }
+
+    public void setId_kategori(int id_kategori) {
+        this.id_kategori = id_kategori;
+    }
+
+    public String getData_leshimit() {
+        return data_leshimit;
+    }
+
+    public void setData_leshimit(String data_leshimit) {
+        this.data_leshimit = data_leshimit;
+    }
+
+    public String getStatusi() {
+        return statusi;
+    }
+
+    public void setStatusi(String statusi) {
+        this.statusi = statusi;
+    }
+
+
 }
 

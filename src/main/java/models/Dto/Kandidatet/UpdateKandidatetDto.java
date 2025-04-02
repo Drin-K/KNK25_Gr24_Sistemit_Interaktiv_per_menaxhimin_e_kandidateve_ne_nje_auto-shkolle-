d366c198481655c39b6568f5fcfb218df0061399
albenaveseli <albena.veseli@student.uni-pr.edu>
2025-04-02 13:00:48 +0200

    krijimi i Dto-ve te tabeles Kandidatet

--- src/main/java/models/Dto/Kandidatet/UpdateKandidatetDto.java
+++ src/main/java/models/Dto/Kandidatet/UpdateKandidatetDto.java
@@ -1,4 +1,32 @@
 package models.Dto.Kandidatet;
 
 public class UpdateKandidatetDto{
+    private String numri_telefonit;
+    private String email;
+    private String adresa;
+    private String statusi_i_procesit;
+
+    public UpdateKandidatetDto(String numri_telefonit, String email, String adresa, String statusi_i_procesit) {
+        this.numri_telefonit = numri_telefonit;
+        this.email = email;
+        this.adresa = adresa;
+        this.statusi_i_procesit = statusi_i_procesit;
+    }
+
+    public String getNumri_telefonit() {
+        return numri_telefonit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public String getStatusi_i_procesit() {
+        return statusi_i_procesit;
+    }
 }
+

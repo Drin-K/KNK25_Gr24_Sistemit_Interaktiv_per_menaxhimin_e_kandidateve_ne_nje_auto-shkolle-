cdf3a48c1b00d11eef879f6df5cd7ad32b7aab79
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-02 13:14:34 +0200

    Krijimi i Kategorite_Patentes.java

--- src/main/java/models/Kategorite_Patentes.java
+++ src/main/java/models/Kategorite_Patentes.java
@@ -1,4 +1,34 @@
 package models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Kategorite_Patentes {
+    private int id_kategori;
+    private String kategoria;
+    private String pershkrimi;
+
+    private Kategorite_Patentes(int id_kategori, String kategoria, String pershkrimi) {
+        this.id_kategori = id_kategori;
+        this.kategoria = kategoria;
+        this.pershkrimi = pershkrimi;
+    }
+    public static Kategorite_Patentes getInstance(ResultSet result) throws SQLException{
+        int id_kategori = result.getInt("id_kategori");
+        String kategoria = result.getString("kategoria");
+        String pershkrimi = result.getString("pershkrimi");
+        return new Kategorite_Patentes(id_kategori,kategoria,pershkrimi);
+    }
+
+    public int getId_kategori() {
+        return id_kategori;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
 }

b63285b3e2118fb104a71238d5fbbc8003165359
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-02 13:07:13 +0200

    Perditsimi i UpdateAutomjetetDto.java

--- src/main/java/models/Dto/Automjetet/UpdateAutomjetetDto.java
+++ src/main/java/models/Dto/Automjetet/UpdateAutomjetetDto.java
@@ -1,4 +1,19 @@
 package models.Dto.Automjetet;
 
 public class UpdateAutomjetetDto {
+    private int id_automjet;
+    private String statusi;
+    private int id_staf;
+
+    public int getId_automjet() {
+        return id_automjet;
+    }
+
+    public String getStatusi() {
+        return statusi;
+    }
+
+    public int getId_staf() {
+        return id_staf;
+    }
 }

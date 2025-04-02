35c64baefd5f0a5de17ae68aa1a0d1d709588bfe
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-02 13:33:46 +0200

    Shtimi i fushes id_kategori dhe getter setter perkates tek UpdateKategorite_PatentesDto.java

--- src/main/java/models/Dto/Kategorite_Patentes/UpdateKategorite_PatentesDto.java
+++ src/main/java/models/Dto/Kategorite_Patentes/UpdateKategorite_PatentesDto.java
@@ -1,7 +1,16 @@
 package models.Dto.Kategorite_Patentes;
 
 public class UpdateKategorite_PatentesDto {
+    private int id_kategori;
     private String pershkrimi;
+
+    public int getId_kategori() {
+        return id_kategori;
+    }
+
+    public void setId_kategori(int id_kategori) {
+        this.id_kategori = id_kategori;
+    }
 
     public String getPershkrimi() {
         return pershkrimi;

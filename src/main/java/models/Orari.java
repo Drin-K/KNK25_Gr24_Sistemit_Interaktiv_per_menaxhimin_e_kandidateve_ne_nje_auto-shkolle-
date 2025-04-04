b63f248f36c6383adc5fa12590badb90d05499ed
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-04 16:13:13 +0200

    Ndrrimi i emertimit

--- src/main/java/models/Orari.java
+++ src/main/java/models/Orari.java
@@ -4,73 +4,74 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Orari {
-    private int id_sesioni;
-    private int id_kandidat;
-    private int id_staf;
-    private String data_sesionit;
-    private String ora_e_fillimit;
-    private String ora_e_perfundimit;
-    private String lloji_i_mesimit;
+    private int idSesioni;
+    private int idKandidat;
+    private int idStaf;
+    private String dataSesionit;
+    private String oraFillimit;
+    private String oraPerfundimit;
+    private String llojiMesimit;
     private String statusi;
-    private int id_automjet;
+    private int idAutomjet;
 
-    private Orari(int id_sesioni, int id_kandidatet, int id_staf, String data_sesionit, String ora_e_fillimit, String ora_e_perfundimit, String lloji_i_mesimit, String statusi, int id_automjet) {
-        this.id_sesioni = id_sesioni;
-        this.id_kandidat = id_kandidatet;
-        this.id_staf = id_staf;
-        this.data_sesionit = data_sesionit;
-        this.ora_e_fillimit = ora_e_fillimit;
-        this.ora_e_perfundimit = ora_e_perfundimit;
-        this.lloji_i_mesimit = lloji_i_mesimit;
-        this.statusi = statusi;
-        this.id_automjet = id_automjet;
-    }
     public static Orari getInstance(ResultSet resultSet)throws SQLException{
-        int id_sesioni= resultSet.getInt("id_sesioni");
-        int id_kanditat=resultSet.getInt("id_kandidat");
-        int id_staf=resultSet.getInt("id_staf");
-        String data_sesionit=resultSet.getString("data_sesionit");
-        String ora_e_fillimit=resultSet.getString("ora_e_fillimit");
-        String ora_e_perfundimit=resultSet.getString("ora_e_perfundimit");
-        String lloji_i_mesimit=resultSet.getString("lloji_i_mesimit");
+        int idSesioni= resultSet.getInt("id_sesioni");
+        int idKanditat=resultSet.getInt("id_kandidat");
+        int idStaf =resultSet.getInt("id_staf");
+        String dataSesionit=resultSet.getString("data_sesionit");
+        String oraFillimit=resultSet.getString("ora_e_fillimit");
+        String oraPerfundimit=resultSet.getString("ora_e_perfundimit");
+        String llojiMesimit=resultSet.getString("lloji_i_mesimit");
         String statusi=resultSet.getString("statusi");
-        int id_automjet=resultSet.getInt("id_automjet");
-        return new Orari(id_sesioni,id_kanditat,id_staf,data_sesionit,ora_e_fillimit,ora_e_perfundimit,lloji_i_mesimit,statusi,id_automjet);
+        int idAutomjet=resultSet.getInt("id_automjet");
+        return new Orari(idSesioni,idKanditat, idStaf,dataSesionit,oraFillimit,oraPerfundimit,llojiMesimit,statusi,idAutomjet);
     }
 
-    public int getId_sesioni() {
-        return id_sesioni;
+    public int getIdSesioni() {
+        return idSesioni;
     }
 
-    public int getId_kandidat() {
-        return id_kandidat;
+    public int getIdKandidat() {
+        return idKandidat;
     }
 
-    public int getId_staf() {
-        return id_staf;
+    public int getIdStaf() {
+        return idStaf;
     }
 
-    public String getData_sesionit() {
-        return data_sesionit;
+    public String getDataSesionit() {
+        return dataSesionit;
     }
 
-    public String getOra_e_fillimit() {
-        return ora_e_fillimit;
+    public String getOraFillimit() {
+        return oraFillimit;
     }
 
-    public String getOra_e_perfundimit() {
-        return ora_e_perfundimit;
+    public String getOraPerfundimit() {
+        return oraPerfundimit;
     }
 
-    public String getLloji_i_mesimit() {
-        return lloji_i_mesimit;
+    public String getLlojiMesimit() {
+        return llojiMesimit;
     }
 
     public String getStatusi() {
         return statusi;
     }
 
-    public int getId_automjet() {
-        return id_automjet;
+    public int getIdAutomjet() {
+        return idAutomjet;
+    }
+
+    private Orari(int idSesioni, int idKandidat, int idStaf, String dataSesionit, String oraFillimit, String oraPerfundimit, String llojiMesimit, String statusi, int idAutomjet) {
+        this.idSesioni = idSesioni;
+        this.idKandidat = idKandidat;
+        this.idStaf = idStaf;
+        this.dataSesionit = dataSesionit;
+        this.oraFillimit = oraFillimit;
+        this.oraPerfundimit = oraPerfundimit;
+        this.llojiMesimit = llojiMesimit;
+        this.statusi = statusi;
+        this.idAutomjet = idAutomjet;
     }
 }

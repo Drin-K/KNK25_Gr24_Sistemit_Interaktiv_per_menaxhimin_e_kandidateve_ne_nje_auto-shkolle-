d6aa52378b12a55c9139ae64e7fdce23339160e7
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-04-04 17:10:33 +0200

    Krijimi i FeedBack.java

--- src/main/java/models/FeedBack.java
+++ src/main/java/models/FeedBack.java
@@ -6,12 +6,52 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class FeedBack {
-    private int idFeedback;
+    private int id;
     private  int idKandidat;
     private int idStaf;
     private LocalDate dataFeedback;
     private int vlersimi;
     private String koment;
 
+    private FeedBack(int id, int idKandidat, int idStaf, LocalDate dataFeedback, int vlersimi, String koment) {
+        this.id = id;
+        this.idKandidat = idKandidat;
+        this.idStaf = idStaf;
+        this.dataFeedback = dataFeedback;
+        this.vlersimi = vlersimi;
+        this.koment = koment;
+    }
+    public static FeedBack getInstance(ResultSet resultSet)throws SQLException{
+        int id=resultSet.getInt("id_feedback");
+        int idKandidat=resultSet.getInt("id_kandidat");
+        int idStaf=resultSet.getInt("id_staf");
+        LocalDate dataFeedback=resultSet.getObject("data_feedback",LocalDate.class);
+        int vlersimi=resultSet.getInt("vleresimi");
+        String koment=resultSet.getString("koment");
+        return new FeedBack(id,idKandidat,idStaf,dataFeedback,vlersimi,koment);
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public int getIdKandidat() {
+        return idKandidat;
+    }
+
+    public int getIdStaf() {
+        return idStaf;
+    }
+
+    public LocalDate getDataFeedback() {
+        return dataFeedback;
+    }
+
+    public int getVlersimi() {
+        return vlersimi;
+    }
+
+    public String getKoment() {
+        return koment;
+    }
 }

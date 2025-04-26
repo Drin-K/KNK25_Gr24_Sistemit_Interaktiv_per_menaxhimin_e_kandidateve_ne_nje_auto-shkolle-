1f02a78b46247b27ffda66355fade6076652cb75
mehmetmehmeti <memimehmet8@gmail.com>
2025-04-26 23:38:29 +0200

    Shtimi i validimeve tek PatentaService

--- src/main/java/services/PatentaService.java
+++ src/main/java/services/PatentaService.java
@@ -1,5 +1,60 @@
 package services;
 
-public class PatentaService {
+import models.Dto.patenta.CreatePatentaDto;
+import models.Patenta;
+import models.Stafi;
+import repository.PatentaRepository;
 
+
+import java.util.ArrayList;
+
+public class PatentaService {
+    private PatentaRepository patentaRepository;
+    private String emailRegEx = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"; //RegEx i thjesht i emailit
+    private String numriTelefonitRegEx = "\\d{8,15}";// nga bardhi idea. Shikoni KandidatetService per me shum info :)
+    private boolean isValidStatus(String status) {
+        return status.equalsIgnoreCase("aktive") ||
+                status.equalsIgnoreCase("e skaduar");
+    }
+    public PatentaService() {
+        this.patentaRepository = new PatentaRepository();
+    }
+    public Patenta getById(int id) throws Exception{
+        if(id<=0){
+            throw new Exception("Id does not exist!");
+        }
+        Patenta patenta = this.patentaRepository.getById(id);
+        if (patenta == null) throw new Exception("Stafi nuk u gjet!");
+        return patenta;
+    }
+    public Patenta create(CreatePatentaDto createDto) throws Exception{
+
+        if (createDto.getIdKandidat() <= 0) {
+            throw new IllegalArgumentException("ID e kandidatit është e pavlefshme!");
+        }
+        if (createDto.getIdKategori() <= 0) {
+            throw new IllegalArgumentException("ID e kategorisë është e pavlefshme!");
+        }
+        if (createDto.getDataLeshimit() == null) {
+            throw new IllegalArgumentException("Data e lëshimit është e detyrueshme!");
+        }
+        if (createDto.getStatusi() == null || createDto.getStatusi().isBlank()) {
+            throw new IllegalArgumentException("Statusi është i detyrueshëm!");
+        }
+        if (!isValidStatus(createDto.getStatusi())) {
+            throw new IllegalArgumentException("Statusi i patentës nuk është i vlefshëm!");
+        }
+       Patenta patenta = patentaRepository.create(createDto);
+        if(patenta == null){
+            throw new Exception("Patenta nuk u krijua");
+        }
+        return patenta;
+    }
+    public ArrayList<Patenta> getAll(){
+        return patentaRepository.getAll(); //Kthen te gjithe kandidatet i nevojshum per listime, grafe, tabela etj....
+    }
+    public boolean delete(int id) throws Exception{
+        this.getById(id); // E kontrollojm a ekziston
+        return patentaRepository.delete(id);
+    }
 }

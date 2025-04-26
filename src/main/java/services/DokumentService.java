3425a1a5bd3b45890e1899531344ed6d9eba8eac
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-04-26 23:21:52 +0200

    Shtim dhe krijimi i DokumentService.java

---
+++ src/main/java/services/DokumentService.java
@@ -0,0 +1,69 @@
+package services;
+
+import models.Dokumentet;
+import models.Dto.dokumentet.CreateDokumentetDto;
+import models.Kandidatet;
+import repository.DokumentetRepository;
+import repository.KandidatetRepository;
+
+public class DokumentService {
+    private DokumentetRepository dokumentRepository;
+    private KandidatetRepository kandidatRepository;
+
+    public DokumentService() {
+        this.dokumentRepository = new DokumentetRepository();
+        this.kandidatRepository = new KandidatetRepository();
+    }
+
+    // Funksioni për krijimin e dokumentit
+    public Dokumentet create(CreateDokumentetDto createDokumentDto) throws Exception {
+        // Validimet bazike për të kontrolluar të dhënat e dokumentit
+        if (createDokumentDto.getIdKandidat() <= 0) {
+            throw new Exception("ID e Kandidatit nuk është valide.");
+        }
+
+        if (createDokumentDto.getLlojiDokumentit() == null || createDokumentDto.getLlojiDokumentit().isEmpty()) {
+            throw new Exception("Lloji i dokumentit është i detyrueshëm.");
+        }
+
+        if (!isValidDokumentType(createDokumentDto.getLlojiDokumentit())) {
+            throw new Exception("Lloji i dokumentit duhet të jetë 'Leternjoftim', 'Certifikate Mjekësore', 'Aplikim', ose 'Foto'.");
+        }
+
+        // Kontrollojmë nëse kandidati ekziston
+        Kandidatet kandidati = this.kandidatRepository.getById(createDokumentDto.getIdKandidat());
+        if (kandidati == null) {
+            throw new Exception("Kandidati me ID: " + createDokumentDto.getIdKandidat() + " nuk ekziston.");
+        }
+
+        // Krijo dokumentin
+        Dokumentet dokument = this.dokumentRepository.create(createDokumentDto);
+        if (dokument == null) {
+            throw new Exception("Dokumenti nuk u krijua me sukses.");
+        }
+
+        return dokument;
+    }
+
+    // Funksioni për të marrë dokumentin me ID
+    public Dokumentet getById(int id) throws Exception {
+        if (id <= 0) {
+            throw new Exception("ID e dokumentit nuk është valide.");
+        }
+
+        Dokumentet dokument = this.dokumentRepository.getById(id);
+        if (dokument == null) {
+            throw new Exception("Nuk u gjet dokumenti me ID: " + id);
+        }
+
+        return dokument;
+    }
+
+    // Funksioni për të kontrolluar nëse lloji i dokumentit është i vlefshëm
+    private boolean isValidDokumentType(String llojiDokumentit) {
+        return llojiDokumentit.equals("Leternjoftim") ||
+                llojiDokumentit.equals("Certifikate Mjekësore") ||
+                llojiDokumentit.equals("Aplikim") ||
+                llojiDokumentit.equals("Foto");
+    }
+}

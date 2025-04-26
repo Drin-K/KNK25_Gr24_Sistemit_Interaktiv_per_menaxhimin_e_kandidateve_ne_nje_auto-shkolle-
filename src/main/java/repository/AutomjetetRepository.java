b6265127d4caed9761b19e2f143f0527ebe2b377
BardhTahiri <bardhtahiri15@gmail.com>
2025-04-26 13:29:05 +0200

    krijimi i update ne AutomjetetRepository.java

--- src/main/java/repository/AutomjetetRepository.java
+++ src/main/java/repository/AutomjetetRepository.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.util.ArrayList;
 
 public class AutomjetetRepository extends BaseRepository<Automjetet, CreateAutomjetetDto, UpdateAutomjetetDto>{
     public AutomjetetRepository() {super("Automjetet");}
@@ -38,6 +39,36 @@ public class AutomjetetRepository extends BaseRepository<Automjetet, CreateAutom
         return null;
         }
         public Automjetet update(UpdateAutomjetetDto automjetetDto){
-        return null;
+            StringBuilder query = new StringBuilder("UPDATE AUTOMJETET SET ");
+            ArrayList<Object> params = new ArrayList<>();
+
+            if(automjetetDto.getIdStaf() != 0){
+                query.append("ID_STAF = ?, ");
+                params.add(automjetetDto.getIdStaf());
+            }
+            if(automjetetDto.getStatusi() != null){
+                query.append("STATUSI = ?, ");
+                params.add(automjetetDto.getStatusi());
+            }
+            if(params.isEmpty()){
+                return getById(automjetetDto.getId());
+            }
+            query.setLength(query.length() -2);
+            query.append(" WHERE ID_AUTOMJET = ?");
+            params.add(automjetetDto.getId());
+            try{
+                PreparedStatement pstm = this.connection.prepareStatement(query.toString());
+                for(int i=0;i<params.size();i++){
+                    pstm.setObject(i+1,params.get(i));
+                }
+                int update = pstm.executeUpdate();
+                if(update == 1){
+                    return this.getById(automjetetDto.getId());
+                }
+            }
+            catch (SQLException e){
+                throw new RuntimeException("Gabim gjate perditsimit te automjetit",e);
+            }
+            return null;
         }
     }

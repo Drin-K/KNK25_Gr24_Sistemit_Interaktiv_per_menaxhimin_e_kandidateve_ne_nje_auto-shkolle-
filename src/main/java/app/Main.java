2737e435cc817e26382661dfe96c32a6c9f43dad
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 00:00:32 +0200

    Lidhja e kontrollerit me main per testim!

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import services.SceneManager;
+import utils.SceneLocator;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource(
+                        SceneLocator.FRONT_PAGE
+                )
+        );
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        SceneManager.initialize(scene);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}

c1cba7efb995ee173c0ca1b3bf8f5931592bf25a
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 01:08:00 +0200

    Shtimi i pathave te FXML!

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -3,6 +3,20 @@ package utils;
 public class SceneLocator {
     public static final String LOGIN_PAGE="/app/login_in.fxml";
     public static final String SIGNUP_PAGE="/app/sign_up.fxml";
-    public static final String FRONT_PAGE="/app/FrontPage.Konsumator.fxml";
-    public static final String CATEGORY_PAGE="/app/candidate_category.fxml";
+    public static final String FRONT_PAGE= "/app/candidateFXML/FrontPage.Konsumator.fxml";
+    public static final String CATEGORY_PAGE= "/app/candidateFXML/candidate_category.fxml";
+    public static final String HOME_PAGE= "/app/candidateFXML/candidate_home.fxml";
+    public static final String SCHEDULER_PAGE= "/app/candidateFXML/candidate_scheduler.fxml";
+    public static final String TESTS_PAGE= "/app/candidateFXML/candidate_test.fxml";
+    public static final String PAYMENTS_PAGE= "/app/candidateFXML/candidate_payments.fxml";
+    public static final String PROGRESS_PAGE= "/app/candidateFXML/candidate_progress.fxml";
+    public static final String FEEDBACK_PAGE= "/app/candidateFXML/candidate_feedback.fxml";
+    public static final String PROFILE_PAGE= "/app/candidateFXML/candidate_profile.fxml";
+
+
+
+
+
+
+
 }

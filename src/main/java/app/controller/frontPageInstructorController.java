fa49dfef2ba53fb66e8993148cc4d617118b9871
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 17:08:54 +0200

    Shtimi i funksioneve click ne controller!

--- src/main/java/app/controller/frontPageInstructorController.java
+++ src/main/java/app/controller/frontPageInstructorController.java
@@ -18,8 +18,39 @@ public class frontPageInstructorController {
     @FXML
     private AnchorPane rightPage;
     @FXML
-    private void schedulerClick()throws Exception{
+    private void homeClick()throws Exception{
+        SceneManager.load(SceneLocator.INSTRUCTOR_HOME,this.rightPage);
+    }
+    @FXML
+    private void scheduleClick()throws Exception{
         SceneManager.load(SceneLocator.INSTRUCTOR_SCHEDULER,this.rightPage);
+    }
+    @FXML
+    private void mngTestClick()throws Exception{
+        SceneManager.load(SceneLocator.MANAGE_TESTS,this.rightPage);
+
+    }
+    @FXML
+    private void mngVehiclesClick()throws Exception{
+        SceneManager.load(SceneLocator.MANAGE_VEHICLES,this.rightPage);
+    }
+    @FXML
+    private void raportsClick()throws Exception{
+        SceneManager.load(SceneLocator.REPORTS,this.rightPage);
+    }
+    @FXML
+    private void feedbackClick()throws Exception{
+        SceneManager.load(SceneLocator.INSTRUCTOR_FEEDBACK,this.rightPage);
+    }
+    @FXML
+    private void profileClick()throws Exception{
+        SceneManager.load(SceneLocator.INSTRUCTOR_PROFILE,this.rightPage);
+    }
+    @FXML
+    private void languageClick()throws Exception{
+        languageManager.getInstance().toggleLanguage();
+        SceneManager.reload();
+        SceneManager.load(SceneLocator.getCurrentRightPage(), rightPage);
     }
 
 

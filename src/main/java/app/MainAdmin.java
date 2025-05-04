a40406acc90aad20a29e629dea93a0d194bcad01
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 14:40:18 +0200

    Krijimi i Main vetem per Admin!

---
+++ src/main/java/app/MainAdmin.java
@@ -0,0 +1,16 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import services.SceneManager;
+import utils.SceneLocator;
+
+import java.io.IOException;
+
+public class MainAdmin extends Application {
+    public void start(Stage stage) throws IOException {
+        SceneManager sceneManager = SceneManager.getInstance(SceneLocator.FRONT_PAGE_ADMIN);
+        stage.setScene(sceneManager.getScene());
+        stage.show();
+    }
+}

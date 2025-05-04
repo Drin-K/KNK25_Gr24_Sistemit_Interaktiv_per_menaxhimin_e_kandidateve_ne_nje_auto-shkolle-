7dd03328dc737856ccd1eb6b9d0a5bc38ac59177
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 14:38:14 +0200

    Krijimi i main vetem per konsumator!

--- src/main/java/app/MainConsumator.java
+++ src/main/java/app/MainConsumator.java
@@ -1,18 +1,15 @@
 package app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import services.SceneManager;
 import utils.SceneLocator;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class MainConsumator extends Application {
     public void start(Stage stage) throws IOException {
-        SceneManager sceneManager = SceneManager.getInstance();
+        SceneManager sceneManager = SceneManager.getInstance(SceneLocator.FRONT_PAGE);
         stage.setScene(sceneManager.getScene());
         stage.show();
     }

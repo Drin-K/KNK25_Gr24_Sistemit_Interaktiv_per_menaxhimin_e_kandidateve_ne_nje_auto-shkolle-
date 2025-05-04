93a270c35a4b43167fda1f77702243d421ffa915
albenaveseli <albena.veseli@student.uni-pr.edu>
2025-05-04 13:42:43 +0200

    reduktim i disa fxml files per adminin per shkak se perfshihen te pjesa e stafit

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -13,6 +13,14 @@ public class SceneLocator {
     public static final String FEEDBACK_PAGE= "/app/candidateFXML/candidate_feedback.fxml";
     public static final String PROFILE_PAGE= "/app/candidateFXML/candidate_profile.fxml";
     private static String currentRightPage = HOME_PAGE;
+
+    //PER ADMIN!
+    public static final String FRONT_PAGE_ADMIN="/app/adminFXML/FrontPage.Admin.fxml";
+    public static final String LICENSES_PAGE="/app/adminFXML/admin_licenses.fxml";
+    public static final String STAF_PAGE="/app/adminFXML/admin_manage_staf.fxml";
+    public static final String CANDIDATES_PAGE="/app/adminFXML/admin_manage_candidates.fxml";
+    public static final String PAYMENTS_MANAGE_PAGE="/app/adminFXML/admin_payments.fxml";
+
     public static void setCurrentRightPage(String path) {
         currentRightPage = path;
     }

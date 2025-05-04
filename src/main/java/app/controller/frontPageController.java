d0b2e2a65ee741cb876df2bba9ac90954c74bae7
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-04 03:45:51 +0200

    Shtimi i languageClick function per ndrrimin e gjuhes!

--- src/main/java/app/controller/frontPageController.java
+++ src/main/java/app/controller/frontPageController.java
@@ -1,13 +1,24 @@
 package app.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import services.LanguageManager;
 import services.SceneManager;
 import utils.SceneLocator;
 
+import java.util.Locale;
+
 public class frontPageController {
+
+    private final LanguageManager languageManager = LanguageManager.getInstance();
+    private void loadLanguage(Locale locale) throws Exception{
+        languageManager.setLocale(locale);
+        SceneManager.reload();
+    }
     @FXML
     private AnchorPane rightPage;
+
     @FXML
     private void categoryClick()throws Exception{
 
@@ -41,5 +52,11 @@ public class frontPageController {
     @FXML
     private void profileClick()throws Exception{
         SceneManager.load(SceneLocator.PROFILE_PAGE,this.rightPage);
+    }
+    @FXML
+    private void languageClick()throws Exception{
+        languageManager.getInstance().toggleLanguage();
+        SceneManager.reload();
+        SceneManager.load(SceneLocator.getCurrentRightPage(), rightPage);
     }
 }

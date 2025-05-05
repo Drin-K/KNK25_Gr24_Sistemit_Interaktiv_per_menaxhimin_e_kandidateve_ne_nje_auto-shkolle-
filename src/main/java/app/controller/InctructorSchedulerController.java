275f763ef20f6e1ec1832bcf69abd9ea8f62ee39
mehmetmehmeti <memimehmet8@gmail.com>
2025-05-05 13:42:12 +0200

    InstructorController krijimi

---
+++ src/main/java/app/controller/InctructorSchedulerController.java
@@ -0,0 +1,11 @@
+package app.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class InctructorSchedulerController {
+
+    @FXML
+    private TextField;
+
+}

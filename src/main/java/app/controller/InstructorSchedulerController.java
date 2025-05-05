5891c60aa6b350b926c3cde08f1399f561bc70ee
mehmetmehmeti <memimehmet8@gmail.com>
2025-05-05 14:12:23 +0200

    InstructorController edit

---
+++ src/main/java/app/controller/InstructorSchedulerController.java
@@ -0,0 +1,58 @@
+package app.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class InstructorSchedulerController {
+
+    @FXML
+    private TextField candidateId;
+    @FXML
+    private DatePicker dateForLesson;
+    @FXML
+    private TextField txtStart;
+    @FXML
+    private TextField txtEnd;
+    @FXML
+    private TextField txtSpecify;
+
+    @FXML
+    private void scheduleClick(){
+
+    }
+    @FXML
+    private void  removeScheduleClick(){
+
+    }
+
+    @FXML
+    private void teoriClick(){
+
+    }
+
+    @FXML
+    private  void praktikClick(){
+
+    }
+
+    @FXML
+    private  void golf4Click(){
+
+    }
+
+    @FXML
+    private  void bmwClick(){
+
+    }
+
+    @FXML
+    private  void mercedesClick(){
+
+    }
+
+    @FXML
+    private  void glof7Click(){
+
+    }
+}

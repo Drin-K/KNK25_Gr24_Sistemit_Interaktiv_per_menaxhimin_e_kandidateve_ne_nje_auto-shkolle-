2f79a6fa1c9fe4e1acebba282b4b4b125896daee
BardhTahiri <bardhtahiri15@gmail.com>
2025-05-13 14:37:07 +0200

    Shtimi i application icon

--- src/main/java/app/MainInstructor.java
+++ src/main/java/app/MainInstructor.java
@@ -1,6 +1,7 @@
 package app;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import services.SceneManager;
 import utils.SceneLocator;
@@ -11,6 +12,8 @@ public class MainInstructor extends Application {
     public void start(Stage stage) throws IOException {
         SceneManager sceneManager = SceneManager.getInstance(SceneLocator.INSTRUCTOR_FRONT_PAGE);
         stage.setScene(sceneManager.getScene());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/Tiger-Logo.jpg")));
+        stage.show();
         stage.show();
     }
 }

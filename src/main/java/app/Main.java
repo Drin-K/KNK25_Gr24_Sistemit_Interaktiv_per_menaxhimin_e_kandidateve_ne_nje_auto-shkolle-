1a0d805e3457bec656c17dfcc17b689f4f07576f
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-15 22:12:22 +0200

    Modifikim i dhe thjeshtesim i kodit!

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,16 +1,13 @@
 package app;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import services.SceneManager;
 import utils.SceneLocator;
 
-import java.io.IOException;
-
-public class MainInstructor extends Application {
+public class Main extends Application {
     public void start(Stage stage) throws Exception {
-        SceneManager.mainLoad(SceneLocator.INSTRUCTOR_FRONT_PAGE,stage);
+        SceneManager.mainLoad(SceneLocator.LOGIN_PAGE,stage);
 
     }
 }

86e42ef27111037dc1666daca1709e0671b38fc2
BardhTahiri <bardhtahiri15@gmail.com>
2025-05-19 14:49:32 +0200

    Fix

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,14 @@
 package app;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import services.SceneManager;
 import utils.SceneLocator;
 
-public class Main extends Application {
+public class  Main extends Application {
     public void start(Stage stage) throws Exception {
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/Tiger-Logo.jpg")));
         SceneManager.mainLoad(SceneLocator.LOGIN_PAGE,stage);
-
     }
 }
